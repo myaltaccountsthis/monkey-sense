d77d441d9d976a35bc38cd361d344d2cd473fcf5
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2025-05-30 23:38:33 -0500

    Leaderboard user link

--- src/components/leaderboard/LeaderboardEntryComponent.tsx
+++ src/components/leaderboard/LeaderboardEntryComponent.tsx
@@ -6,5 +6,5 @@ interface LeaderboardEntryComponentProps {
 };
 
 export default function LeaderboardEntryComponent({entry, i}: LeaderboardEntryComponentProps) {
-    return <tr key={i}><td>{entry.username}</td><td>{entry.correct}</td><td>{entry.answered}</td><td>{entry.adjusted}</td><td>{entry.time.toFixed(1)}</td></tr>
+    return <tr key={i}><td><a className="text-inherit hover:text-gray-200" href={`/users/${entry.user_id}`}>{entry.username}</a></td><td>{entry.correct}</td><td>{entry.answered}</td><td>{entry.adjusted}</td><td>{entry.time.toFixed(1)}</td></tr>
 }
